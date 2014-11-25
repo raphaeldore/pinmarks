bf285365fec09c13bd1387c779eed9b72163d7c9
Raphaël Doré <rdore@neomalibox.ch>
2014-11-25 17:07:19 -0500

    Basic dropwizard project setup

---
+++ src/main/java/ca/csf/rdore/pinmarks/PinmarksApplication.java
@@ -0,0 +1,34 @@
+package ca.csf.rdore.pinmarks;
+
+import io.dropwizard.Application;
+import io.dropwizard.setup.Bootstrap;
+import io.dropwizard.setup.Environment;
+import ca.csf.rdore.pinmarks.resources.PinmarksResource;
+
+// import ca.csf.rdore.pinmarks.health.TemplateHealthCheck;
+
+public class PinmarksApplication extends Application<PinmarksConfiguration> {
+  public static void main(String[] args) throws Exception {
+    new PinmarksApplication().run(args);
+  }
+
+  @Override
+  public String getName() {
+    return "Hello, World!";
+  }
+
+  @Override
+  public void initialize(Bootstrap<PinmarksConfiguration> arg0) {
+    // TODO Auto-generated method stub
+
+  }
+
+  @Override
+  public void run(PinmarksConfiguration configuration, Environment environment) throws Exception {
+    final PinmarksResource resource =
+        new PinmarksResource(configuration.getTemplate(), configuration.getDefaultName());
+    environment.jersey().register(resource);
+
+  }
+
+}

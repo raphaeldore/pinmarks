b75606eda51f80902a85e7f7517f043caadc57e0
Raphaël Doré <rdore@neomalibox.ch>
2014-11-28 12:45:28 -0500

    Now load the assets, and the resources.

--- src/main/java/ca/csf/rdore/pinmarks/PinmarksApplication.java
+++ src/main/java/ca/csf/rdore/pinmarks/PinmarksApplication.java
@@ -1,9 +1,12 @@
 package ca.csf.rdore.pinmarks;
 
 import io.dropwizard.Application;
+import io.dropwizard.assets.AssetsBundle;
 import io.dropwizard.setup.Bootstrap;
 import io.dropwizard.setup.Environment;
+import io.dropwizard.views.ViewBundle;
 import ca.csf.rdore.pinmarks.health.TemplateHealthCheck;
+import ca.csf.rdore.pinmarks.resources.IndexResource;
 import ca.csf.rdore.pinmarks.resources.PinmarksResource;
 
 // import ca.csf.rdore.pinmarks.health.TemplateHealthCheck;
@@ -19,19 +22,24 @@ public class PinmarksApplication extends Application<PinmarksConfiguration> {
   }
 
   @Override
-  public void initialize(Bootstrap<PinmarksConfiguration> arg0) {
-    // TODO Auto-generated method stub
-
+  public void initialize(Bootstrap<PinmarksConfiguration> bootstrap) {
+    bootstrap.addBundle(new AssetsBundle("/assets/bootstrap/css", "/css", null, "css"));
+    bootstrap.addBundle(new AssetsBundle("/assets/bootstrap/js", "/js", null, "js"));
+    bootstrap.addBundle(new AssetsBundle("/assets/bootstrap/fonts", "/fonts", null, "fonts"));
+    
+    bootstrap.addBundle(new ViewBundle());
   }
 
   @Override
   public void run(PinmarksConfiguration configuration, Environment environment) throws Exception {
     final PinmarksResource resource =
         new PinmarksResource(configuration.getTemplate(), configuration.getDefaultName());
+    final IndexResource indexResource = new IndexResource();
     final TemplateHealthCheck healthCheck = new TemplateHealthCheck(configuration.getTemplate());
 
     environment.healthChecks().register("template", healthCheck);
     environment.jersey().register(resource);
+    environment.jersey().register(indexResource);
   }
 
 }

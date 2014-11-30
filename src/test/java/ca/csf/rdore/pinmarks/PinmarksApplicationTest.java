2164517dc9d2cf4796ef51fa7b603b190baae6fe
Raphaël Doré <rdore@neomailbox.ch>
2014-11-29 21:29:01 -0500

    Non working tests for now. I'm just setting up everything for latter. Had to add dropwizard-testing dependency to pom

---
+++ src/test/java/ca/csf/rdore/pinmarks/PinmarksApplicationTest.java
@@ -0,0 +1,33 @@
+package ca.csf.rdore.pinmarks;
+
+import io.dropwizard.jersey.setup.JerseyEnvironment;
+import io.dropwizard.setup.Environment;
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.reset;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class PinmarksApplicationTest {
+  private final Environment environment = mock(Environment.class);
+  private final JerseyEnvironment jersey = mock(JerseyEnvironment.class);
+  private final PinmarksApplication application = new PinmarksApplication();
+  private final PinmarksConfiguration config = new PinmarksConfiguration();
+  
+  @Before
+  public void setup() throws Exception {
+      //config.setMyParam("yay");
+      when(environment.jersey()).thenReturn(jersey);
+  }
+
+  @Test
+  public void buildsAThingResource() throws Exception {
+      // application.run(config, environment);
+
+      //verify(jersey).register(any(ThingResource.class));
+  }
+
+}

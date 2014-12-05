61cec1612956f25a490e0597cb6e3b215fede611
Raphaël Doré <rdore@neomalibox.ch>
2014-12-05 12:53:08 -0500

    Search is implemented.

--- src/main/java/ca/csf/rdore/pinmarks/resources/IndexResource.java
+++ src/main/java/ca/csf/rdore/pinmarks/resources/IndexResource.java
@@ -1,18 +1,20 @@
 package ca.csf.rdore.pinmarks.resources;
 
+import java.util.List;
+
+import javax.ws.rs.DefaultValue;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 
+import ca.csf.rdore.pinmarks.core.Bookmark;
 import ca.csf.rdore.pinmarks.daos.BookmarkDAO;
 import ca.csf.rdore.pinmarks.daos.TagDAO;
-import ca.csf.rdore.pinmarks.views.BookmarksView;
 import ca.csf.rdore.pinmarks.views.IndexView;
 
 import com.codahale.metrics.annotation.Timed;
-
-import io.dropwizard.views.*;
 
 @Path("/")
 public class IndexResource {
@@ -28,14 +30,15 @@ public class IndexResource {
   @GET
   @Produces(MediaType.TEXT_HTML)
   @Timed
-  public IndexView index() {
+  public IndexView index(@QueryParam("search") String searchPattern, @DefaultValue("title") @QueryParam("searchBy") String searchBy) {
+    
+    // User is searching, only display the search results
+    if (searchPattern != null) {
+      return new IndexView(bookmarkDao.findBookmarksByPattern(searchBy , "%" + searchPattern + "%"));
+    }
+    
+    // Just load the index with everything
     return new IndexView(bookmarkDao.getAllBookmarks());
   }
-
-/*  @GET
-  public BookmarksView getBookmarks() {
-    BookmarksResource bookmarksResource = new BookmarksResource(bookmarkDao, tagDao);
-    return bookmarksResource.getBookmarks();
-  }*/
 
 }

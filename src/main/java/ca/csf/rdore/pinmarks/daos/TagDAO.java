78c3cc58591502c8cef01dd4d860c494f4ec7d12
Raphaël Doré <rdore@neomalibox.ch>
2014-12-05 19:41:31 -0500

    Get the IDs of bookmarks with the searched tags

--- src/main/java/ca/csf/rdore/pinmarks/daos/TagDAO.java
+++ src/main/java/ca/csf/rdore/pinmarks/daos/TagDAO.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.skife.jdbi.v2.sqlobject.Bind;
 import org.skife.jdbi.v2.sqlobject.BindBean;
 import org.skife.jdbi.v2.sqlobject.SqlBatch;
+import org.skife.jdbi.v2.sqlobject.SqlQuery;
 
 import ca.csf.rdore.pinmarks.core.Tag;
 
@@ -14,5 +15,8 @@ public interface TagDAO {
   
   @SqlBatch("INSERT INTO TAG (tag_text, bookmark_id) values (:tagText, :bookmarkID)")
   void insertTagsBean(@BindBean List<Tag> bookmarkTags);
+  
+  @SqlQuery("SELECT tag.bookmark_id FROM tag WHERE (tag.tag_text IN :it) GROUP BY tag.bookmark_id")
+  List<Integer> findBookmarksByTag(@Bind String tags);
   
 }
